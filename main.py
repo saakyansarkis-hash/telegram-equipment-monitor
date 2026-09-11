6c333d12ed9a8a399f18cc29ad7bf410218b2ef4
saakyansarkis-hash <saakyansarkis@icloud.com>
2026-09-11 20:26:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,36 @@ from telethon import TelegramClient, events
 
 api_id = int(os.getenv("TELEGRAM_API_ID"))
 api_hash = os.getenv("TELEGRAM_API_HASH")
+
 client = TelegramClient("monitor", api_id, api_hash)
+
 keywords = [
-"нужен экскаватор-погрузчик",
-"ищу экскаватор-погрузчик",
-"экскаватор-погрузчик",
-"нужен каток",
-"нужен самосвал",
-"нужны самосвалы",
-"ищу самосвал",
-"требуется самосвал",
-"нужен мини-погрузчик",
-"нужен мини погрузчик",
-"ищу мини-погрузчик",
-"ищу мини погрузчик",
-"требуется мини-погрузчик",]
+    "нужен экскаватор-погрузчик",
+    "ищу экскаватор-погрузчик",
+    "экскаватор-погрузчик",
+    "нужен каток",
+    "нужен самосвал",
+    "нужны самосвалы",
+    "ищу самосвал",
+    "требуется самосвал",
+    "нужен мини-погрузчик",
+    "нужен мини погрузчик",
+    "ищу мини-погрузчик",
+    "ищу мини погрузчик",
+    "требуется мини-погрузчик",
+]
+
+
 @client.on(events.NewMessage)
 async def handler(event):
-   text = (event.message.message or "").lower()
-  for word in keywords:
-       if word in text:
-         print("Найдена заявка:")
-         print(event.message.message)
-         break
+    text = (event.message.message or "").lower()
+
+    for word in keywords:
+        if word in text:
+            print("Найдена заявка:")
+            print(event.message.message)
+            break
+
+
 client.start()
 client.run_until_disconnected()
